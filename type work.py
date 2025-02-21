67ce72c5b8937e603e1d411544df8047700eb233
Senna <150617119+sennamacc@users.noreply.github.com>
2025-02-21 14:28:59 -0500

    Started by asking for user input and defined some variables

---
+++ type work.py
@@ -0,0 +1,16 @@
+"""
+Have that program declare a type of each type of variable we mentioned in class and print it to screen with a message declaring what it is.
+
+Write a comment above each variable describing the data type
+
+Difficulty Mark 2
+"""
+integer = int(1)
+float = float(2/3)
+string = str("cupcake")
+
+type = input("What type of variable would you like to learn about today?")
+type.lower
+if type == int or type == integer
+    print("ok")
+else print("Invalid selection.")
